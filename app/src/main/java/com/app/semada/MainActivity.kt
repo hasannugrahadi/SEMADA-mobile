d4f437330c9e9890ef850413515340fb948d6ff8
Rahmatullah <e41221006@student.polije.ac.id>
2023-11-17 06:13:30 +0700

    keempat
    layouting login, orientasi potrait

--- app/src/main/java/com/app/semada/MainActivity.kt
+++ app/src/main/java/com/app/semada/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // menyembunyikan
+        supportActionBar?.hide()
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
